f65e6f33a30e15ee9a27c4e97e4770eb605f10a8
ViniTex231 <viniteixeira2828@gmail.com>
2023-07-12 14:00:31 -0300

    finish

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var jogador = 'x'
+
+function jogar(celula){
+    if (celula.innerHTML == ""){
+        celula.innerHTML = jogador
+        if(jogador == 'x'){
+            jogador = 'o';
+        } else {
+            jogador = 'x'
+        }
+    }
+}
+
+function reiniciar(){
+    location.reload()
+}
